0d18220f30ad4a5c6d08677e4bbc7a1dff8a961c
crystalqingyu <crystalqingyu@163.com>
2015-06-09 09:56:07 +0800

    新建项目single view，把没用的都给删除了

---
+++ LjjWeibo/LjjWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LjjWeibo
+//
+//  Created by liujingjing on 15/6/9.
+//  Copyright (c) 2015年 liujingjing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LjjWeibo/LjjWeibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LjjWeibo
+//
+//  Created by liujingjing on 15/6/9.
+//  Copyright (c) 2015年 liujingjing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
